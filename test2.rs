1f7ebc33336d236626447bcff1919f5b2de92690
Shreyan Jain <shreyan11@duck.com>
2026-04-12 23:23:42 +0530

    Create test2.rs

---
+++ test2.rs
@@ -0,0 +1,12 @@
+fn main() {  
+    let x = 5;  
+    let y = 10;  
+      
+    if x < y {  
+        println!("x is less than y");  
+    }  
+      
+    for i in 0..5 {  
+        println!("Number: {}", i);  
+    }  
+}
